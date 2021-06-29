d0df6153360e1de30d1be6588d9c858f5542e416
nishanthvg <nishanthvg33@gmail.com>
2021-06-29 14:22:34 +0530

    week3-over

---
+++ R_prog/week3/assignment.R
@@ -0,0 +1,39 @@
+makeCacheMatrix <- function(m = matrix()) {
+  i <- NULL
+  
+  set <- function(matrix) {
+    m <<- matrix
+    i <- NULL
+  }
+  
+  get <- function() {
+    m
+  }
+  
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  getInverse <- function() {
+    i 
+  }
+  
+  list(set= set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...){
+  m <- x$setInverse()
+  
+  if(!is.null(m)){
+    message("Getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  
+  m
+}
+
